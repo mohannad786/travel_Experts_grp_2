b9defadbd1cec1a6ef4673346abe708df8cbc015
mohannad786 <83979479+mohannad786@users.noreply.github.com>
2021-08-05 15:51:25 -0600

    Fixed index page message and details button

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const mongo = require("mongodb").MongoClient;
 const uniqueValidator = require("mongoose-unique-validator");
+// Generate random Greeting Program
+var myarray = ['ready for some exciting time', 'how are you today', 'good to see you', 'hope you are having a good day']
 
 
 function getPackages(pid, callback) {
@@ -14,7 +16,7 @@ function getPackages(pid, callback) {
       const db = client.db("userdata");  // get reference to the DB
       const collection = db.collection("packages"); // Get reference to the collection
       const query = {}    // Empty query to get all data
-      if (pid) query.packid = parseInt(pid);  // If you need certain product
+      if (pid) query.packid = (pid);  // If you need certain product
       collection.find(query).toArray((err, items) => {
         // console.log(query)
         // if (err) { throw err; }
@@ -34,20 +36,26 @@ router.get('/', function (req, res, next) {
   getPackages(null, // null => get all packages
     function (err, packdata) { // Callback function, will be called when the data from the DB is ready
       if (err) throw err
+      const greet = Math.floor(Math.random() * myarray.length);
+      var greetings = myarray[greet];
+      console.log(myarray[greet]);
       // Render the PUG template with the product data we got from the DB
        res.render('index', {
-        mypackages: packdata
+        mypackages: packdata,
+        greet_msg: ('Welcome to our travel Portal, '+greetings ),
+        date_time: ('You have logged in on,' +(new Date()).toString()),
       });
     }
   );
 });
 
 /* GET one product listing. */
-router.get('/:packageid', function (req, res, next) {
+router.get('/details/:packageid', function (req, res, next) {
   const packid = req.params.packageid
   console.log(packid);
   getPackages(packid,
     function (err, data) {
+ 
       if (err) throw err
       res.render('index', {
         mypackages: data,
@@ -59,20 +67,6 @@ router.get('/:packageid', function (req, res, next) {
 });
 
 
-// // Generate random Greeting Program
-// var myarray = ['ready for some exciting time', 'how are you today', 'good to see you', 'hope you are having a good day']
-// const greet = Math.floor(Math.random() * myarray.length);
-// var greetings = myarray[greet];
-// console.log(myarray[greet]);
-
-  
-// // /* GET home page and display greeting message */
-// // router.get('/', function(req, res, next) {
-// //   res.render('index', { 
-// //     greet_msg: ('Welcome to our travel Portal,'+greetings ),
-// //     date_time: (new Date()).toString(),
-// //   });
-// // });
 
 
 module.exports = router;
